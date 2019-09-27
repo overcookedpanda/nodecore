dec1ffd5342c7a6c90bdb2e9cef02caf267d18ca
Andrew Lekar <andrey_lekar@adoriasoft.com>
2019-09-28 00:31:20 +0200

    Remove unrelated calls

--- nodecore-miners-pop/src/main/java/nodecore/miners/pop/Configuration.java
+++ nodecore-miners-pop/src/main/java/nodecore/miners/pop/Configuration.java
@@ -286,11 +286,6 @@ public class Configuration {
     public String getCronSchedule() {
         return getPropertyOverrideOrDefault(Keys.SCHEDULE);
     }
-    
-    @Override
-    public String getCronRebootSchedule() {
-        return getPropertyOverrideOrDefault(Keys.REBOOT_SCHEDULE);
-    }
 
     public int getActionTimeout() {
         Integer timeout = Ints.tryParse(getPropertyOverrideOrDefault(Keys.ACTION_TIMEOUT));
@@ -406,13 +401,6 @@ public class Configuration {
                 result.addMessage("V052",
                         "Runtime configuration not allowed",
                         String.format("Property '%s' cannot be changed at runtime. Edit properties file and restart.", Keys.SCHEDULE),
-                        false);
-                result.fail();
-                break;
-            case Keys.REBOOT_SCHEDULE:
-                result.addMessage("V052",
-                        "Runtime configuration not allowed",
-                        String.format("Property '%s' cannot be changed at runtime. Edit properties file and restart.", Keys.REBOOT_SCHEDULE),
                         false);
                 result.fail();
                 break;
@@ -566,7 +554,6 @@ public class Configuration {
 
         private static final String SCHEDULE = "pop.cron.schedule";
         private static final String ACTION_TIMEOUT = "pop.action.timeout";
-        private static final String REBOOT_SCHEDULE = "pop.cron.schedule.reboot";
 
         private static List<String> getAll() {
             List<String> keys = new ArrayList<>();
@@ -587,7 +574,6 @@ public class Configuration {
             keys.add(NODECORE_CERT_CHAIN_PATH_KEY);
             keys.add(SCHEDULE);
             keys.add(ACTION_TIMEOUT);
-            keys.add(REBOOT_SCHEDULE);
             return keys;
         }
     }
